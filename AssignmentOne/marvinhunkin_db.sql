679b062a8f88f6c2bcda0e18f2ec815859d23ee2
asifjan <asif.ihsan@gmail.com>
2013-11-26 21:23:22 +0500

    db

--- AssignmentOne/marvinhunkin_db.sql
+++ AssignmentOne/marvinhunkin_db.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 69.163.163.74:
--- Generation Time: Nov 25, 2013 at 08:55 PM
--- Server version: 5.1.53-log
--- PHP Version: 5.3.27
+-- Host: localhost
+-- Generation Time: Nov 26, 2013 at 09:21 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `marvinhunkin_db`
+-- Database: `contactcentre`
 --
 
 -- --------------------------------------------------------
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `tcontact`
 --
 
-DROP TABLE IF EXISTS `tcontact`;
 CREATE TABLE IF NOT EXISTS `tcontact` (
   `contact_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -59,7 +58,6 @@ INSERT INTO `tcontact` (`contact_id`, `name`, `address`, `town`, `state`, `post_
 -- Table structure for table `tevent`
 --
 
-DROP TABLE IF EXISTS `tevent`;
 CREATE TABLE IF NOT EXISTS `tevent` (
   `event_id` int(11) NOT NULL AUTO_INCREMENT,
   `notes` varchar(255) NOT NULL,
@@ -69,14 +67,16 @@ CREATE TABLE IF NOT EXISTS `tevent` (
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`event_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `tevent`
 --
 
 INSERT INTO `tevent` (`event_id`, `notes`, `date_entered`, `time_entered`, `contact_id`, `user_id`) VALUES
-(1, 'Progress of web site and need to review code by November 30, 2013. ', '2013-08-08', '09:18:00', NULL, 0);
+(1, 'Progress of web site and need to review code by November 30, 2013. ', '2013-08-08', '09:18:00', NULL, 0),
+(2, 'notes', '2013-11-26', '19:30:23', 2, 0),
+(3, 'test', '2013-11-26', '19:41:41', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -84,23 +84,23 @@ INSERT INTO `tevent` (`event_id`, `notes`, `date_entered`, `time_entered`, `cont
 -- Table structure for table `tlogin`
 --
 
-DROP TABLE IF EXISTS `tlogin`;
 CREATE TABLE IF NOT EXISTS `tlogin` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `date_accessed` date NOT NULL,
+  `role` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `tlogin`
 --
 
-INSERT INTO `tlogin` (`user_id`, `user_name`, `email`, `password`, `date_accessed`) VALUES
-(1, 'StarTrekCafe', 'startrekcafe@gmail.com', 'password', '2013-08-08'),
-(2, 'UserName', 'email@yahoo.com', 'password', '0000-00-00');
+INSERT INTO `tlogin` (`user_id`, `user_name`, `email`, `password`, `date_accessed`, `role`) VALUES
+(1, 'StarTrekCafe', 'startrekcafe@gmail.com', 'password', '2013-08-08', 'User'),
+(2, 'UserName', 'email@yahoo.com', 'password', '0000-00-00', 'Admin');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
