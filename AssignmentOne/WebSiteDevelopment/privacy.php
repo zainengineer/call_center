311d5d9a0cf52c5d39afaabadbe2c7a5b808fd78
asifjan <asif.ihsan@gmail.com>
2013-11-26 22:24:36 +0500

    header / footer update

--- AssignmentOne/WebSiteDevelopment/privacy.php
+++ AssignmentOne/WebSiteDevelopment/privacy.php
@@ -1,25 +1,8 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
-<title>Contact Centre Privacy</title>
-<link rel="stylesheet" href="style.css" type="text/css" title="contact-centre" />
-<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-</head>
-<body class="contact">
-<h1>Contact Centre</h1>
-<div id="jump">
-<a href="#jumps">jump to content</a>
-</div>
-<nav>
-<ul>
-<li><a href="index.html">Home</a></li>
-<li><a href="accounts.html">Accounts</a></li>
-<li><a href="contacts.html">Contacts</a></li>
-<li><a href="events.html">Events</a></li>
-</ul>
-</nav>
+<?php
+require_once (dirname(__FILE__) . '/lib/common.php');
+//forceUserLogin();
+include  (dirname(__FILE__) . '/header.php');
+?>
 <div class="layout-grid">
 <div id="jumps" class="layout-cell layout-1">
 <div>
@@ -29,14 +12,7 @@
 </div>
 </div>
 </div><!--/grid-->
-<nav>
-<ul>
-<li><a href="copywrite.html">Copyright</a></li>
-<li><a href="disclaimer.html">Disclaimer</a></li>
-<li><a href="privacy.html">Privacy</a></li>
-<li><a href="feedback.html">Feedback</a></li>
-</ul>
-</nav>
-</body>
-</html>
 
+<?php
+include  (dirname(__FILE__)  . '/footer.php');
+?>
